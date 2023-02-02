b24cec08607f243c1598982cb867d879f83374e6
Pedro Fernandes <pedrofpereira222@gmail.com>
2023-02-02 15:52:46 -0300

    new
    nerw

---
+++ sintaxe-variaveis-e-fluxo/src/testaVariaveis.java
@@ -0,0 +1,8 @@
+public class testaVariaveis {
+    public static void main(String[] args) {
+        int idade = 28;
+        var nome = "Pedro ";
+        double flutuante = 2.33;
+        System.out.println(nome + idade);
+    }
+}
