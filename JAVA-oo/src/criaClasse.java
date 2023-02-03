e77356f8cca572b054b84c91267b772828d1c2bf
Pedro Fernandes <pedrofpereira222@gmail.com>
2023-02-03 11:50:20 -0300

    novo exercicio

--- JAVA-oo/src/criaClasse.java
+++ JAVA-oo/src/criaClasse.java
@@ -5,6 +5,16 @@ public class criaClasse {
         Pedro.agencia = "1195";
         Pedro.numero = "439293";
         Pedro.titular = "Pedro";
-        System.out.printf("Meu nome é %s, agencia é %s, numero é %s, saldo é %,.2f", Pedro.titular, Pedro.agencia, Pedro.numero, Pedro.saldo);
+        System.out.printf("Meu nome é %s, agencia é %s, numero é %s, saldo é %,.2f",
+                Pedro.titular,
+                Pedro.agencia,
+                Pedro.numero,
+                Pedro.saldo);
+        Conta Gabriel = new Conta();
+        Gabriel.saldo = 200;
+        Gabriel.titular = "Gabriel";
+        Gabriel.numero = "5555";
+        Gabriel.agencia = "321321";
+
     }
 }
