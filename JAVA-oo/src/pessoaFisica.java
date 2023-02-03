e7fc93fb507d87fe2ea287ee487fdc205cbc8c15
Pedro Fernandes <pedrofpereira222@gmail.com>
2023-02-03 15:12:22 -0300

    novo exercicio

---
+++ JAVA-oo/src/pessoaFisica.java
@@ -0,0 +1,36 @@
+public class pessoaFisica {
+    private String nome;
+
+    public pessoaFisica(String nome, String id, String CPF) {
+        this.nome = nome;
+        this.id = id;
+        this.CPF = CPF;
+    }
+
+    private String id;
+    private String CPF;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+}
