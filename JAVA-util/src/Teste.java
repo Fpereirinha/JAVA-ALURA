0098c594ebd0e23f11f661cae52f6ace85c74c1f
Pedro Fernandes <pedrofpereira222@gmail.com>
2023-02-14 12:12:52 -0300

    novo exercicio

--- JAVA-util/src/Teste.java
+++ JAVA-util/src/Teste.java
@@ -1,21 +1,23 @@
-
-
 import ByteBank.Modelo.Conta;
+import ByteBank.Modelo.contaCorrente;
 import ByteBank.Modelo.contaPoupanca;
+
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+
 
 public class Teste {
 
     public static void main(String[] args) {
 
-        Conta cc1 = new contaPoupanca();
+        Conta cc1 = new contaCorrente();
         cc1.deposita(333.0);
 
         Conta cc2 = new contaPoupanca();
         cc2.deposita(444.0);
 
-        Conta cc3 = new contaPoupanca();
+        Conta cc3 = new contaCorrente();
         cc3.deposita(111.0);
 
         Conta cc4 = new contaPoupanca();
@@ -26,7 +28,36 @@ public class Teste {
         lista.add(cc2);
         lista.add(cc3);
         lista.add(cc4);
+        for (Conta conta : lista) {
+            System.out.println(conta.getSaldo());
+        }
+
+        NumeroDaContaComparator comparator = new NumeroDaContaComparator();
+
+        System.out.println("---------");
+
+        lista.sort(new Comparator<Conta>() {
+
+            @Override
+            public int compare(Conta c1, Conta c2) {
+
+                return Double.compare(c1.getSaldo(), c2.getSaldo());
+            }
+        });
+
+        for (Conta conta : lista) {
+            System.out.println(conta.getSaldo());
+        }
 
     }
 
+}
+
+class NumeroDaContaComparator implements Comparator<Conta> {
+
+    @Override
+    public int compare(Conta c1, Conta c2) {
+
+        return Double.compare(c1.getSaldo(), c2.getSaldo());
+    }
 }
